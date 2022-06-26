4431dd086ced00233657b08f028d3ecc95c3a31a
TerriHack <a.dubuc@rubika-edu.com>
2022-06-26 16:20:03 +0200

    UI Part 4
    -Leantween Added
    -Main Menu Animation Added
    -QuitPopUp Added

--- StageHFI/Assets/UI/Scripts/MainMenu.cs
+++ StageHFI/Assets/UI/Scripts/MainMenu.cs
@@ -1,16 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   void Start()
+    [Header("Animation Curves")]
+    public LeanTweenType curveIn;
+    public LeanTweenType curveOut;
+
+    [Space]
+    [SerializeField]
+    private GameObject quitPopUp;
+    [SerializeField]
+    private RectTransform background;
+
+    
+    private void Start()
     {
+        // Reset Scale
+        quitPopUp.transform.localScale = Vector3.zero;
         
+        // Reset scale & alpha
+        LeanTween.scale(background, Vector3.zero, 0);
+        LeanTween.alpha(background, 0, 0);
+    }
+    
+    public void StartGame()
+    {
+        // SceneManager.LoadScene("INSERT SCENE NAME");
     }
 
-    void Update()
+    public void OpenQuitPopUp()
     {
+        LeanTween.scale(quitPopUp,Vector3.one, 0.4f).setEase(curveIn);
         
+        // Make the background appear (scale & alpha)
+        LeanTween.scale(background, Vector3.one, 0);
+        LeanTween.alpha(background, 0.4f, 0.4f).setEase(curveIn);
     }
+    
+    public void CloseQuitPopUp()
+    {
+        LeanTween.scale(quitPopUp,Vector3.zero, 0.4f).setEase(curveIn);
+        
+        // Make the background disappear (scale & alpha)
+        LeanTween.scale(background, Vector3.zero, 0);
+        LeanTween.alpha(background, 0, 0.4f).setEase(curveIn);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
 }
---
+++ StageHFI/Assets/UI/Scripts/OptionsTween.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class OptionsTween : MonoBehaviour
+{
+
+    [Header("Animation Curves")]
+    public LeanTweenType curveIn;
+    public LeanTweenType curveOut;
+    
+    [Space]
+    [SerializeField]
+    private RectTransform background;
+    
+    private void Start()
+    {
+        // Reset Scale
+        transform.localScale = Vector3.zero;
+        
+        // Reset scale & alpha
+        LeanTween.scale(background, Vector3.zero, 0);
+        LeanTween.alpha(background, 0, 0);
+    }
+
+    public void OpenOptions()
+    {
+        LeanTween.scale(gameObject,Vector3.one, 0.4f).setEase(curveIn);
+        
+        // Make the background appear (scale & alpha)
+        LeanTween.scale(background, Vector3.one, 0);
+        LeanTween.alpha(background, 0.4f, 0.4f).setEase(curveIn);
+    }
+    
+    public void CloseOptions()
+    {
+        LeanTween.scale(gameObject,Vector3.zero, 0.4f).setEase(curveOut);
+        
+        // Make the background disappear (scale & alpha)
+        LeanTween.scale(background, Vector3.zero, 0);
+        LeanTween.alpha(background, 0, 0.4f).setEase(curveOut);
+    }
+}
